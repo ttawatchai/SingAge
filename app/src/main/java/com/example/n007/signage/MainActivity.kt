219975f198be88cadd0b5dfdf697c471905e59d8
Tawatchai <ttawatchai.job@gmail.com>
2018-07-12 18:19:28 +0700

    [Add]  vdo

--- app/src/main/java/com/example/n007/signage/MainActivity.kt
+++ app/src/main/java/com/example/n007/signage/MainActivity.kt
@@ -1,7 +1,8 @@
 package com.example.n007.signage
 
 import android.annotation.SuppressLint
-import android.media.MediaPlayer
+import android.content.pm.ActivityInfo
+import android.content.res.Configuration
 import android.os.Bundle
 import android.os.Handler
 import android.support.v7.app.AppCompatActivity
@@ -11,14 +12,13 @@ import android.view.WindowManager
 import com.example.n007.signage.model.Info.content
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
-import android.media.MediaPlayer.OnPreparedListener
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var mHandler: Handler
     private lateinit var mRunnable: Runnable
     var contentList: MutableList<content> = arrayListOf()
-    var path = "android.resource://com.example.n007.signage/" + R.raw.vdo1
+    var uriPath = "android.resource://com.example.n007.signage/raw/"
     var count: Int = 0
     var time: Int = 0
 
@@ -28,13 +28,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN)
-
-
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
         mHandler = Handler()
         addData()
         setTime()
     }
-
     @SuppressLint("SetTextI18n")
     private fun setTime() {
         mRunnable = Runnable {
@@ -55,10 +53,17 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun addData() {
-        contentList.add(content("pic1", "pic", "11:43:1"))
-        contentList.add(content("pic2", "pic", "11:44:2"))
-        contentList.add(content("pic3", "pic", "11:47:3"))
-        contentList.add(content("vdo", "vdo", "11:45:4"))
+        contentList.add(content("vdo1", "vdo", "09:00:00"))
+        contentList.add(content("pic1", "pic", "09:10:00"))
+        contentList.add(content("pic2", "pic", "09:20:00"))
+        contentList.add(content("vdo2", "vdo", "09:30:00"))
+        contentList.add(content("pic3", "pic", "11:00:00"))
+        contentList.add(content("vdo3", "vdo", "11:10:00"))
+        contentList.add(content("pic4", "pic", "11:20:00"))
+        contentList.add(content("pic5", "pic", "11:30:00"))
+        contentList.add(content("pic6", "pic", "13:00:00"))
+        contentList.add(content("vdo4", "vdo", "13:10:00"))
+        contentList.add(content("pic7", "pic", "13:20:00"))
     }
 
     @SuppressLint("ResourceType")
@@ -69,7 +74,7 @@ class MainActivity : AppCompatActivity() {
                 if (i._type.equals("vdo")) {
                     show_img.visibility = View.GONE
                     show_vdo.visibility = View.VISIBLE
-                    show_vdo.setVideoPath(path)
+                    show_vdo.setVideoPath(uriPath + contentList.get(time)._name)
                     show_vdo.start()
                 } else if (i._type.equals("pic")) {
                     show_vdo.visibility = View.GONE
@@ -85,31 +90,33 @@ class MainActivity : AppCompatActivity() {
     @SuppressLint("ResourceType")
     private fun checkTimeTest() {
         Log.d("pic", time.toString() + "count" + count)
-        count++
         if (time == contentList.size) {
             time = 0
         }
 
-        if (count == 30 || count == 0) {
+        if (count == 15 || count == 0) {
             count = 0
             var id = resources.getIdentifier(contentList.get(time)._name, "raw", packageName)
             if (contentList.get(time)._type.equals("vdo")) {
                 show_img.visibility = View.GONE
                 show_vdo.visibility = View.VISIBLE
                 show_vdo.setOnPreparedListener { mp -> mp.isLooping = true }
-                show_vdo.setVideoPath(path)
+                show_vdo.setVideoPath(uriPath + contentList.get(time)._name)
                 show_vdo.start()
 
             } else if (contentList.get(time)._type.equals("pic")) {
                 show_vdo.visibility = View.GONE
                 show_img.visibility = View.VISIBLE
                 show_img.setBackgroundResource(id)
-
             }
             time++
-
         }
+        count++
+    }
 
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        setContentView(R.layout.activity_main)
     }
 
 
